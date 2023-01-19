2f1829051cb85d02ffce5f91d7ea8bb96be0115f
Gwendal <gwetre@gmail.com>
2023-01-19 14:41:11 +0100

    start methods added

--- README.md
+++ README.md
@@ -7,9 +7,14 @@
 
 ## Startup
 
-Execute the main file using either of the commands\
+You can simply run start.sh by clicking on it.\
+\
+Otherwise you can execute the main file using either of the commands\
 ```python main.py```\
     or\
 ```python3 main.py```\
     or\
-```./main.py```
+```./main.py```\
+\
+You might need to give access permissions with\
+```chmod 744 main.py```
---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if command -v python
+then
+    python main.py
+    echo python
+
+else
+    if command -v python3
+    then
+        echo python3
+    else
+        notify-send -u critical "You need to install python3"
+        exit 1
+    fi
+fi
+if command -v pip
+then
+    if pip show pygame
+    then
+        python3 main.py
+    else
+        notify-send -u critical "You need to install pygame"
+    fi
+else
+    notify-send -u critical "You need to install pip"
+fi
